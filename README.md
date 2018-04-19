74e8b5cf129f829a2281478215f8d9cb0508d974
Liao Zhuodi <liao_zd@hotmail.com>
2018-04-19 18:15:40 +0800

    2.1 插入排序python

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CLRS 《算法导论》笔记，代码实现与习题
 
+
+# 相关资源
+
+[MIT课程官网](https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-introduction-to-algorithms-sma-5503-fall-2005/)
+
+[习题答案](http://sites.math.rutgers.edu/~ajl213/CLRS/CLRS.html)
--- ch02_getting_started/2.1_insertion_sort.py
+++ ch02_getting_started/2.1_insertion_sort.py
@@ -1,3 +1,14 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 A = [5, 2, 4, 6, 1, 3]
+
+for j in range(1, len(A)):
+    key = A[j]
+    i = j - 1
+    print(key),
+    while i >= 0 and A[i] > key:
+        A[i+1] = A[i]
+        i = i - 1
+    A[i+1] = key
+
+print(A)
