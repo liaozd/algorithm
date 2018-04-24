6dca93dbed5d456efb7ce3297ca2f427d429007f
Liao Zhuodi <liao_zd@hotmail.com>
2018-04-24 20:53:11 +0800

    update with js code

---
+++ ch02_getting_started/2.1_insertion_sort.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+let A = [5, 2, 4, 6, 1, 3];
+
+for (let j = 1; j < A.length; j++) {
+    let key = A[j];
+    let i = j - 1;
+    while ( i >= 0 && A[i] > key) {
+        A[i + 1] = A[i];
+        i--;
+    }
+    A[i+1] = key;
+}
+console.log(A);
